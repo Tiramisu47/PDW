f035cae874b584f3c36f74edb24ba890de4c135e
Tiramisu47 <Tiramisu47@users.noreply.github.com>
2024-11-17 17:45:46 +0100

    działająca FP camera
    pozdrawiam, działa na TestScene

--- game/Assets/Natalia/PlayerMove.cs
+++ game/Assets/Natalia/PlayerMove.cs
@@ -1,3 +1,4 @@
+using Cinemachine.Utility;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,21 @@ public class PlayerMove : MonoBehaviour
 {
     [Header("Movement")]
     public float moveSpeed;
+
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
     public Transform orientation;
 
     float horizontalInput, verticalInput;
@@ -14,6 +30,8 @@ public class PlayerMove : MonoBehaviour
 
     private void Start()
     {
+        readyToJump = true;
+
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true; // zeby gracz sie nie przewracal przy poruszaniu
     }
@@ -21,6 +39,13 @@ public class PlayerMove : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+        // when to jump
+        if (Input.GetKey(KeyCode.Space) && readyToJump && grounded)
+        {
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
     }
 
     private void MovePlayer()
@@ -28,14 +53,55 @@ public class PlayerMove : MonoBehaviour
         // obliczanie kierunku ruchu gracza
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
         rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+
+        // kiedy na ziemi
+        if (grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        
+        // kiedy w powietrzu
+        else if(!grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
     }
 
     private void Update()
     {
+        // groundcheck
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
         MyInput();
+        SpeedControl();
+
+        // handle drag
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
+        
     }
     private void FixedUpdate()
     {
         MovePlayer();
+    }
+
+    private void SpeedControl() // by postac nie przyspieszala poza wyznaczona predkosc
+    {
+        Vector3 flatVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // ograniczenie velocity
+        if(flatVelocity.magnitude > moveSpeed)
+        {
+            Vector3 limitedVelocity = flatVelocity.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVelocity.x, rb.velocity.y, limitedVelocity.z);
+        }
+    }
+
+    private void Jump()
+    {
+        // reset y velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    private void ResetJump()
+    {
+        readyToJump = true;
     }
 }
