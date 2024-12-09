9739b8348be610ef5e6c7f9975957b315b4a2922
PreOL <149583089+PreOL@users.noreply.github.com>
2024-12-09 13:27:38 +0100

    assetsout
    check

--- game/Assets/Patryk/menuLogic.cs
+++ game/Assets/Patryk/menuLogic.cs
@@ -9,7 +9,7 @@ public class menuLogic : MonoBehaviour
     [SerializeField] private GameObject m3;
     [SerializeField] private GameObject s1;
     [SerializeField] private GameObject s2;
-    [SerializeField] private GameObject s3;
+    
     private void Start()
     {
         m1.SetActive(true);
@@ -17,7 +17,7 @@ public class menuLogic : MonoBehaviour
         m3.SetActive(false);
         s1.SetActive(false);
         s2.SetActive(false);
-        s3.SetActive(false);
+        
 
     }
     public void Credits()
@@ -38,7 +38,7 @@ public class menuLogic : MonoBehaviour
         m2.SetActive(true);
         s1.SetActive(true);
         s2.SetActive(false);
-        s3.SetActive(false);
+        
     }
     public void BackSelect()
     {
@@ -48,20 +48,15 @@ public class menuLogic : MonoBehaviour
     public void DisplaySelect()
     {
         s2.SetActive(true);
-        s3.SetActive(false);
+       
         s1.SetActive(false);
     }
     public void GraphicsSelect()
     {
         s1.SetActive(true);
         s2.SetActive(false);
-        s3.SetActive(false);
+       
     }
-    public void ControlSelect()
-    {
-        s3.SetActive(true);
-        s1.SetActive(false);
-        s2.SetActive(false);
-    }
+ 
 
 }
