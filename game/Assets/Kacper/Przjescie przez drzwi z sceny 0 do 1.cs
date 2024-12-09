1bea150458920b5b870f7781f6117f2bc12cbea5
Keroczkaq <Kerok@op.pl>
2024-12-09 16:09:02 +0100

    Gracz,budynek,poziom
    Przejście przez drzwi między poziomami 0 i 1, budynek, poziom 0, i pierwsze ustawienie gracza

---
+++ game/Assets/Kacper/Przjescie przez drzwi z sceny 0 do 1.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // Umo¿liwia ³adowanie poziomów
+
+public class DoorInteraction : MonoBehaviour
+{
+    public string nextSceneName; // Nazwa sceny, któr¹ chcemy za³adowaæ
+    private bool isPlayerInRange = false; // Flaga okreœlaj¹ca, czy gracz jest w zasiêgu drzwi
+
+    void Update()
+    {
+        // Sprawdza, czy gracz jest w zasiêgu drzwi i czy nacisn¹³ klawisz "E"
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    // Funkcja do ³adowania nastêpnej sceny
+    void LoadNextLevel()
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+
+    // Funkcja uruchamiana, gdy gracz wchodzi w collider drzwi
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+            Debug.Log("Mo¿esz wejœæ przez drzwi. Naciœnij 'E'.");
+        }
+    }
+
+    // Funkcja uruchamiana, gdy gracz opuszcza collider drzwi
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            Debug.Log("Wyszed³eœ z obszaru interakcji drzwi.");
+        }
+    }
+}
